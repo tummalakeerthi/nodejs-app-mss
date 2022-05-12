60ea68695d4dad8d89d090597a7315b822c9e4bf
tummalakeerthi <50941798+tummalakeerthi@users.noreply.github.com>
2022-05-12 20:44:01 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,13 +6,10 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectKey': 'example1',
 	    'sonar.login': '1e3f497876dcf043af233271cc5aa055fc45f355',
-	    //'sonar.login': 'admin',
-	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
-	    'sonar.language':'js',
+	    //'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
-            'sonar.sources': '.',
-	  //'sonar.tests': 'specs',
-          //'sonar.inclusions' : 'src/**'
+            'sonar.sources': '.'
+	     'sonar.exclusions':
        },
 }, () => {});
